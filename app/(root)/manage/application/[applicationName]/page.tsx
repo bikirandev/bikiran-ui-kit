9f780b9740dd423f620d93f18d8e81fffc63e632
Kazal Ghosh <mrgkazal@gmail.com>
2025-03-13 11:43:06 +0600

    delete old file

--- app/(root)/manage/application/[applicationName]/page.tsx
+++
@@ -1,7 +0,0 @@
-import ApplicationInfoPage from "@/bikiran/components/manage-application-info/ApplicationInfoPage";
-import React, { FC } from "react";
-const page: FC = () => {
-  return <ApplicationInfoPage />;
-};
-
-export default page;
