032b34a94e5ca962b557cebf9633a8985fbe0661
Kazal Ghosh <mrgkazal@gmail.com>
2025-03-15 13:36:56 +0600

    add sidebar

--- bikiran/components/user/UserPage.tsx
+++ bikiran/components/user/UserPage.tsx
@@ -1,10 +1,36 @@
+import { icons } from "@/bikiran/lib/icons";
+import Image from "next/image";
 import React from "react";
+import { menuArray } from "./constant";
 
 const UserPage = () => {
   return (
-    <div className="w-[230px] h-screen bg-[#07090B] text-primary-100">
-      <h2>Users</h2>
-      <section className="">left</section>
+    <div className="w-[230px] h-screen bg-[#07090B] text-primary-100 ">
+      {menuArray.map((item) => (
+        <ul key={item.id} className="px-2.5 pt-4">
+          <li className="relative group flex gap-2.5 p-2 ">
+            <div className="w-[26px]">
+              <Image
+                src={item.iconLine}
+                alt="alt"
+                width={0}
+                height={0}
+                sizes="100vw"
+                className="w-full h-auto opacity-100 group-hover:opacity-0"
+              />
+              <Image
+                src={item.iconFill}
+                alt="alt"
+                width={0}
+                height={0}
+                sizes="100vw"
+                className="w-[26px] absolute top-2 opacity-0 group-hover:opacity-100"
+              />
+            </div>
+            <h2>{item.title}</h2>
+          </li>
+        </ul>
+      ))}
     </div>
   );
 };
---
+++ bikiran/components/user/constant.ts
@@ -0,0 +1,70 @@
+import { icons } from "@/bikiran/lib/icons";
+
+
+export const menuArray = [
+    {
+        id: "user",
+        miniTitle: "User",
+        title: "User",
+        iconLine: icons.iconSidebarUser,
+        iconFill: icons.iconSidebarUserFill,
+    },
+    {
+        id: "domain",
+        miniTitle: "domain",
+        title: "Domain",
+        iconLine: icons.iconSidebarDomain,
+        iconFill: icons.iconSidebarDomainFill,
+    },
+    {
+        id: "hosting",
+        miniTitle: "hosting",
+        title: "Hosting",
+        iconLine: icons.iconSidebarHosting,
+        iconFill: icons.iconSidebarHostingFill,
+    },
+    {
+        id: "appOcean",
+        miniTitle: "appOcean",
+        title: "AppOcean",
+        iconLine: icons.iconSidebarSupport,
+        iconFill: icons.iconSidebarSupportFill,
+    },
+    {
+        id: "billing",
+        miniTitle: "billing",
+        title: "Billings",
+        iconLine: icons.iconSidebarBilling,
+        iconFill: icons.iconSidebarBillingFill,
+    },
+    {
+        id: "manage",
+        miniTitle: "manage",
+        title: "Manage",
+        iconLine: icons.iconSidebarManage,
+        iconFill: icons.iconSidebarManageFill,
+    },
+    {
+        id: "support",
+        miniTitle: "support",
+        title: "Support",
+        iconLine: icons.iconSidebarSupport,
+        iconFill: icons.iconSidebarSupportFill,
+    },
+    {
+        id: "logs",
+        miniTitle: "logs",
+        title: "Logs",
+        iconLine: icons.iconSidebarLog,
+        iconFill: icons.iconSidebarLogFill,
+    },
+    {
+        id: "emailNotification",
+        miniTitle: "emailNotification",
+        title: "Email Notification",
+        iconLine: icons.iconSidebarEmailNotification,
+        iconFill: icons.iconSidebarEmailNotificationFill,
+    }
+]
+
+
