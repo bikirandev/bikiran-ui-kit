363c595f6f1293467f8f307ab2e3730396e866b2
Kazal Ghosh <mrgkazal@gmail.com>
2025-03-15 16:55:08 +0600

    add sub nav with specific nav

--- bikiran/components/user/UserPage.tsx
+++ bikiran/components/user/UserPage.tsx
@@ -1,14 +1,30 @@
-import { icons } from "@/bikiran/lib/icons";
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { menuArray } from "./constant";
 
 const UserPage = () => {
+  // const [active, setActive] = useState(false);
+
+  // const handlePricingDetails = () => {
+  //   setActive((prev) => !prev);
+  // };
+
+  const [activeNavId, setActiveNavId] = useState<number | null>(null);
+
+  const handleNavClick = (id: number) => {
+    // Toggle submenu: if the same nav is clicked again, close it
+    setActiveNavId(activeNavId === id ? null : id);
+  };
+
   return (
     <div className="w-[230px] h-screen bg-[#07090B] text-primary-100 ">
       {menuArray.map((item) => (
         <ul key={item.id} className="px-2.5 pt-4">
-          <li className="relative group flex gap-2.5 p-2 ">
+          <li
+            onClick={() => handleNavClick(item.id)}
+            className="relative group flex gap-2.5 p-2 cursor-pointer"
+          >
             <div className="w-[26px]">
               <Image
                 src={item.iconLine}
@@ -29,6 +45,23 @@ const UserPage = () => {
             </div>
             <h2>{item.title}</h2>
           </li>
+
+          {/* Submenu */}
+
+          {activeNavId === item.id && item.subMenu && (
+            <ul
+              className={`pl-8 mt-2 space-y-1 submenu ${activeNavId === item.id ? "open" : ""}`}
+            >
+              {item.subMenu?.map((subItem, index) => (
+                <li
+                  key={index}
+                  className="p-2 text-sm text-gray-300 hover:bg-gray-700 rounded"
+                >
+                  {subItem.title}
+                </li>
+              ))}
+            </ul>
+          )}
         </ul>
       ))}
     </div>
