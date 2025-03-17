0702eccf7692f151098a8066d9b5852bfd4636d4
Kazal Ghosh <mrgkazal@gmail.com>
2025-03-17 12:27:41 +0600

    add animation with submenu

--- bikiran/shared/sideNav/SideNavListComp.tsx
+++ bikiran/shared/sideNav/SideNavListComp.tsx
@@ -19,7 +19,7 @@ const SideNavListComp = () => {
         <ul key={item.id} className="px-2.5 pt-4">
           <li
             onClick={() => handleNavClick(item.id)}
-            className={`relative group flex gap-2.5 p-2 cursor-pointer ${activeNavId === item.id ? "bg-slate-900" : ""}`}
+            className={`relative z-10 group flex gap-2.5 p-2 cursor-pointer ${activeNavId === item.id ? "bg-slate-900" : ""}`}
           >
             <div className="flex gap-2.5 ">
               <div className=" w-[26px]">
@@ -45,56 +45,65 @@ const SideNavListComp = () => {
           </li>
 
           {/* Submenu */}
-          {activeNavId === item.id && item.subMenu && (
-            <ul className="mt-2 transition-all duration-500 ml-5 border-0">
-              {item.subMenu?.map((subItem: any, index: any) => (
-                <li
-                  key={index}
-                  className="relative group cursor-pointer pl-[18px] text-gray-300"
-                >
-                  <div className="absolute left-0 -top-[26px] h-full w-[1px] bg-gray-400" />
-                  <svg
-                    className="absolute left-0 top-[35%] -translate-y-1/2"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 28 20"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
+          <div
+            className={`overflow-hidden transition-all duration-1000 ${
+              activeNavId ? "max-h-[1000px] opacity-100" : "max-h-0  opacity-0"
+            }`}
+          >
+            {activeNavId === item.id && item.subMenu && (
+              <ul className="mt-2 ml-5 border-0">
+                {item.subMenu?.map((subItem: any, index: any) => (
+                  <li
+                    key={index}
+                    className="relative group cursor-pointer pl-[18px] text-gray-300"
                   >
-                    <path
-                      d="M0 0 Q0 20 20 20 H28"
-                      stroke="#ccc"
-                      strokeWidth="1.5"
+                    <div className="absolute left-0 -top-[26px] h-full w-[1px] bg-gray-400" />
+
+                    <svg
+                      className="absolute left-0 top-[36%] -translate-y-1/2"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 28 20"
                       fill="none"
-                    />
-                  </svg>
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M0 0 Q0 20 20 20 H28"
+                        stroke="#ccc"
+                        strokeWidth="1.5"
+                        fill="none"
+                      />
+                    </svg>
 
-                  <div className="flex gap-1.5 hover:bg-red-800 p-2 rounded">
-                    <div className="w-[20px]">
-                      <Image
-                        src={subItem.iconL}
-                        alt="alt"
-                        width={0}
-                        height={0}
-                        sizes="100vw"
-                        className="w-full h-auto opacity-100 group-hover:opacity-0"
-                      />
-                      <Image
-                        src={subItem.iconF}
-                        alt="alt"
-                        width={0}
-                        height={0}
-                        sizes="100vw"
-                        className="w-[20px] absolute top-2 opacity-0 group-hover:opacity-100"
-                      />
+                    <div className="relative z-10 flex gap-1.5 hover:bg-red-800 p-2 rounded">
+                      <div className="w-[20px]">
+                        <Image
+                          src={subItem.iconL}
+                          alt="alt"
+                          width={0}
+                          height={0}
+                          sizes="100vw"
+                          className="w-full h-auto opacity-100 group-hover:opacity-0"
+                        />
+                        <Image
+                          src={subItem.iconF}
+                          alt="alt"
+                          width={0}
+                          height={0}
+                          sizes="100vw"
+                          className="w-[20px] absolute top-2 opacity-0 group-hover:opacity-100"
+                        />
+                      </div>
+
+                      <span className="text-ellipsis overflow-hidden whitespace-nowrap">
+                        {subItem.title}
+                      </span>
                     </div>
-
-                    {subItem.title}
-                  </div>
-                </li>
-              ))}
-            </ul>
-          )}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
         </ul>
       ))}
     </div>
